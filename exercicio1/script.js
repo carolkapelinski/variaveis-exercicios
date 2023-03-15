dd7fb9aca1fca0e38e7780a17d80d585b69130aa
carolkapelinski <carolkapelinkski@gmail.com>
2023-03-15 19:26:00 -0300

    Exercicios resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+const pergunta1 = "Qual seu filme de terror preferido?"
+const pergunta2 = "Qual seu genero musical favorito?"
+const pergunta3 = "Qual sua comfort food preferida?"
+
+const resposta1 = prompt(pergunta1)
+const resposta2 = prompt(pergunta2)
+const resposta3 = prompt(pergunta3)
+
+console.log(pergunta1, resposta1)
+console.log(pergunta2, resposta2)
+console.log(pergunta3, resposta3)
---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+let a = 10
+let b = 25
+let c
+
+c = a
+a = b
+b = c
+
+console.log("O novo resultado de a é: ", a)
+console.log("O novo resultado de b é: ", b)
